4b0c6efd1ac8153a7d091ddd1eba49dc8868e86d
Kevin Strong <kevin@gomerits.com>
2021-07-06 14:50:03 -0500

    Convert 2 tests to table tests

--- weather_test.go
+++ weather_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"weather"
@@ -13,60 +14,98 @@ import (
 )
 
 func TestEndToEnd(t *testing.T) {
-	t.Parallel()
-	server := httptest.NewTLSServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		//goland:noinspection SpellCheckingInspection
-		expected := "/weather?q=Millbrae%2C+CA%2C+USA&appid=fakeAPIKey&units=imperial"
-		if request.RequestURI != expected {
-			t.Fatalf("got \n%s\n but expected \n%s", request.RequestURI, expected)
-		}
-		response, err := os.Open("testData/weather.json")
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = io.Copy(writer, response)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}))
-	service := weather.New(
-		"fakeAPIKey",
-		weather.WithBaseURL(server.URL),
-		weather.WithHTTPClient(server.Client()),
-	)
-	response, err := service.GetWeather("Millbrae, CA, USA")
-	if err != nil {
-		t.Fatal(err)
+	type testCase struct {
+		name            string
+		location        string
+		expectedURL     string
+		expectedWeather weather.CurrentWeather
 	}
-	expect := weather.CurrentWeather{Temp: 56.21, Summary: "Clouds"}
-	if !cmp.Equal(expect, response) {
-		t.Fatal(cmp.Diff(expect, response))
+	//goland:noinspection SpellCheckingInspection
+	testCases := []testCase{
+		{
+			name:        "London",
+			location:    "London, UK",
+			expectedURL: "/weather?q=London%2C+UK&appid=fakeAPIKey&units=imperial",
+			expectedWeather: weather.CurrentWeather{
+				Temp:    56.21,
+				Summary: "Clouds",
+			},
+		},
+		{
+			name:        "Millbrae",
+			location:    "Millbrae, CA, USA",
+			expectedURL: "/weather?q=Millbrae%2C+CA%2C+USA&appid=fakeAPIKey&units=imperial",
+			expectedWeather: weather.CurrentWeather{
+				Temp:    70.8,
+				Summary: "Sunny",
+			},
+		},
+	}
+	for _, test := range testCases {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			server := httptest.NewTLSServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				if request.RequestURI != test.expectedURL {
+					t.Fatalf("got \n%s\n but expected \n%s", request.RequestURI, test.expectedURL)
+				}
+				var weatherFile = "testData/weather.json"
+				if strings.Contains(request.RequestURI, "Millbrae") {
+					weatherFile = "testData/weather_millbrae.json"
+				}
+
+				response, err := os.Open(weatherFile)
+				if err != nil {
+					t.Fatal(err)
+				}
+				_, err = io.Copy(writer, response)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}))
+			service := weather.New(
+				"fakeAPIKey",
+				weather.WithBaseURL(server.URL),
+				weather.WithHTTPClient(server.Client()),
+			)
+			response, err := service.GetWeather(test.location)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !cmp.Equal(test.expectedWeather, response) {
+				t.Fatal(cmp.Diff(test.expectedWeather, response))
+			}
+		})
 	}
 }
 
 func TestRequestGeneration(t *testing.T) {
 	t.Parallel()
-	expected := `https://api.openweathermap.org/data/2.5/weather?q=London%2C+UK&appid=fakeAPIKey&units=imperial`
-	input := "London, UK"
-	service := weather.New("fakeAPIKey",
-		weather.WithBaseURL("https://api.openweathermap.org/data/2.5"),
-	)
-	url := service.MakeURL(input)
-	if !cmp.Equal(url, expected) {
-		t.Fatalf("got \n%s\n but expected \n%s", url, expected)
+	type testCase struct {
+		name            string
+		input           string
+		expectedRequest string
 	}
-}
-
-func TestMillbrae(t *testing.T) {
-	t.Parallel()
-	expected := `https://api.openweathermap.org/data/2.5/weather?q=Millbrae%2C+CA%2C+USA&appid=fakeAPIKey&units=imperial`
-	input := "Millbrae, CA, USA"
-	service := weather.New("fakeAPIKey",
-		weather.WithBaseURL("https://api.openweathermap.org/data/2.5"),
-	)
-	url := service.MakeURL(input)
-	if !cmp.Equal(url, expected) {
-		t.Fatalf("got \n%s\n but expected \n%s", url, expected)
+	tests := []testCase{{
+		name:            "London",
+		input:           "London, UK",
+		expectedRequest: "https://api.openweathermap.org/data/2.5/weather?q=London%2C+UK&appid=fakeAPIKey&units=imperial",
+	},
+		{
+			name:            "Millbrae",
+			input:           "Millbrae, CA",
+			expectedRequest: "https://api.openweathermap.org/data/2.5/weather?q=Millbrae%2C+CA&appid=fakeAPIKey&units=imperial",
+		},
+	}
+	for _, test := range tests {
+		test := test
+		service := weather.New("fakeAPIKey",
+			weather.WithBaseURL("https://api.openweathermap.org/data/2.5"),
+		)
+		url := service.MakeURL(test.input)
+		if !cmp.Equal(url, test.expectedRequest) {
+			t.Fatalf("got \n%s\n but expected \n%s", url, test.expectedRequest)
+		}
 	}
 }
 
@@ -74,7 +113,7 @@ func TestCreateWeatherService(t *testing.T) {
 	t.Parallel()
 	weatherService := weather.New("fakeApiKey")
 	if weatherService.APIKey != "fakeApiKey" {
-		t.Fatal("Failed to create weather service with provided ApiKey")
+		t.Fatal("Failed to create weather service with provided APIKey")
 	}
 }
 
